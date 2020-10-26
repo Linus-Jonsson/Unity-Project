df8fdcfc9e474e8c7fcfda4603e68fe01a756a28
SeasterSwe <Jakobbulow01@gmail.com>
2020-10-26 15:11:34 +0100

    a

--- Fundamentals 15/Assets/Scripts/JakobTempMovement.cs
+++ Fundamentals 15/Assets/Scripts/JakobTempMovement.cs
@@ -38,6 +38,5 @@ public class JakobTempMovement : MonoBehaviour
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
-        transform.rotation *= Quaternion.Euler(0, 0, 90);
     }
 }
