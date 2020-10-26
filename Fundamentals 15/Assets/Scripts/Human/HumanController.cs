d7ac9a3feebfea3cd09758c27a444a498a05bc52
Jackey <jaclun2003@skola.goteborg.se>
2020-10-26 15:06:18 +0100

    Created human which follow player

---
+++ Fundamentals 15/Assets/Scripts/Human/HumanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour {
+	public Transform target;
+
+	[SerializeField] private int speed = 2;
+	private Rigidbody2D rb2D;
+
+	// Start is called before the first frame update
+	private void Start() {
+		target = GameObject.FindWithTag("Player").transform;
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() {
+		Vector3 direction = target.position - transform.position;
+		if (direction.sqrMagnitude > 1)
+			direction.Normalize();
+
+		rb2D.velocity = direction * speed;
+
+		RotateTo(direction);
+	}
+
+	private void RotateTo(Vector3 direction) {
+		Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+		transform.rotation = rotation * Quaternion.Euler(0, 0, 90);
+	}
+}
