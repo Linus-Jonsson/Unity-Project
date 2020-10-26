a313cda7392b0f4795768b1db2b240297338d58d
Linus-Jonsson <linus_jonsson@hotmail.com>
2020-10-26 15:13:41 +0100

    Update - Tranform instead of GameObject

--- Fundamentals 15/Assets/Scripts/ZombieMovement.cs
+++ Fundamentals 15/Assets/Scripts/ZombieMovement.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class ZombieMovement : MonoBehaviour
 {
     [SerializeField] private int speed = 1;
-    [SerializeField] GameObject player;
+    Transform playerTransform;
 	private Rigidbody2D rb2D;
 	private Vector2 direction;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		playerTransform = GameObject.FindWithTag("Player").transform;
 		rb2D = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		direction = new Vector2(player.transform.position.x - transform.position.x,
-								player.transform.position.y - transform.position.y);
+		direction = playerTransform.position - transform.position;
 		direction.Normalize();
 
 		Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
