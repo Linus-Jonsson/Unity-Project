8bb771cf378a0921d2cf90acfbc76e09dbd4a9bf
SeasterSwe <Jakobbulow01@gmail.com>
2020-10-26 14:41:34 +0100

    ShootingStuff
    awrfawef

---
+++ Fundamentals 15/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = Vector2.zero;
+        Destroy(rb);
+        Destroy(GetComponent<Collider2D>());
+        Destroy(GetComponent<TrailRenderer>());
+        transform.parent = collision.transform;
+
+        //om collison tag = Zombie
+        //Zombie.cure
+    }
+}
---
+++ Fundamentals 15/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject shootEffekt;
+    public GameObject bullet;
+    public float launchForce;
+    public Transform shootPoint;
+
+    [Range(0.2f, 1f)]
+    public float fireRate = 0.5f;
+    private float nextFire = 0;
+
+    void Update()
+    {
+        if(Time.time > nextFire && Input.GetMouseButtonDown(0)){
+            nextFire = fireRate + Time.time;
+            GameObject shootEffektClone = Instantiate(shootEffekt, shootPoint.position, shootEffekt.transform.rotation);
+            SpreadShoot(1);
+        }
+    }
+
+    void SpreadShoot(int n, float angleToDivide = 45)
+    {
+        float spreadAngle = angleToDivide / n; //blir vinkel mellan bullets;
+        for (int i = 0; i < n; i++)
+        {
+            float startAngle = (n - 1) * spreadAngle / 2; //gör så att det går att spawna på båda sidor -1 för mitten ska va 0
+            GameObject bulletClone = Instantiate(bullet, shootPoint.position,
+                shootPoint.rotation * Quaternion.Euler(0, 0, -startAngle + spreadAngle * i));
+           
+            Rigidbody2D cloneRb = bulletClone.GetComponent<Rigidbody2D>();
+           
+            //tyckte detta va smart. Stoppar att de åker igenom saker ifall man skjuter för snabbt
+            if (launchForce >= 10) 
+                bulletClone.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+            cloneRb.velocity = bulletClone.transform.up.normalized * launchForce; //transform.up kan va vad som.
+        }
+    }
+}
