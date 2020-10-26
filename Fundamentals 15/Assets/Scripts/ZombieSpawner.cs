6692f8840f4572210ed223ddc11a15814fec083f
Linus-Jonsson <linus_jonsson@hotmail.com>
2020-10-26 23:41:18 +0100

    ZombieSpawner

---
+++ Fundamentals 15/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 3f;
+    [SerializeField] GameObject zombie;
+    bool spawn = true;
+
+    void Start()
+    {
+        StartCoroutine(ZombieSpawn());
+    }
+
+    IEnumerator ZombieSpawn()
+    {
+        while (spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnZombie();
+        }
+    }
+    
+    void SpawnZombie()
+    {
+        GameObject newZombie = Instantiate (zombie, transform.position, transform.rotation) as GameObject;
+        newZombie.transform.parent = transform;
+    }
+}
