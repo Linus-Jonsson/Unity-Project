c55e04c2712466e4d99d7188d92b2feb7614c222
SeasterSwe <Jakobbulow01@gmail.com>
2020-10-28 11:09:45 +0100

    Stor Zombie :de:

---
+++ Fundamentals 15/Assets/LargeZombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeZombie : ZombieMovement
+{
+
+    Camera main;
+    void Start()
+    {
+        main = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dist = Vector2.Distance(transform.position, main.transform.position);
+        if(dist < 6)
+        {
+            main.GetComponent<CameraShake>().ShakeCamera();
+        }
+    }
+}
