335bb733f9154de4b436429d399d62f12fa5de58
Jackey <jaclun2003@skola.goteborg.se>
2020-10-29 11:33:32 +0100

    Now a chopper rescues escorted humans

---
+++ Fundamentals 15/Assets/Scripts/Safezone/Spin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+	[SerializeField] private int spinSpeed = 20;
+
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(Vector3.forward, spinSpeed);
+	}
+}
